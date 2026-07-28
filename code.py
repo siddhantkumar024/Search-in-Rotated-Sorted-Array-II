192bf8f73dbaac188a2729902bcc335f2f0af8d1
SIDDHANT KUMAR <76165102+siddhantkumar024@users.noreply.github.com>
2026-07-28 19:25:07 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,7 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        for num in nums:
+            if num==target:
+                return True
+        return False
+        
